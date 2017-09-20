accf8d9d83fc8b8f4cfffa962ac53d09d042cb7f
Leo Knudsen <nightskater-220@hotmail.com>
2017-09-20 16:09:26 +0200

    add theme support hookup

---
+++ functions.php
@@ -0,0 +1,26 @@
+<?php 
+
+/**
+ * add various theme support
+ */
+function add_themes_support() {
+	/**
+	 * register navigation menus
+	 */
+
+	/**
+	 * register post formats
+	 */
+	
+	/**
+	 * register support for post thumbnails
+	 */
+}
+
+// hook the theme support
+add_action('after_setup_theme', 'add_themes_support');
+
+
+
+
+ ?>
